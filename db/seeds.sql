1aa053896fd7db1332184e34b2a2d2b3d43d4e56
danman15726 <danman15726@gmail.com>
2020-01-08 22:56:03 -0600

    Intialized files and created schema and seed sql files. Also installed necessary node packages.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ("Bourbon Burger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("Portabelo Mushroom/Onion", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("Bacon Wrapped Wagyu Beef/Aged Gouda", false);
