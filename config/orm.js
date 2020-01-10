c674a698342be84ecc1aef88858a3ed6269694a1
danman15726 <danman15726@gmail.com>
2020-01-10 15:28:27 -0600

    finished the ORM methods in the ORM.js file. I used a orm.js file from the class repo for reference. changed some variable names. Forced myself to understand why it does what it does.

--- config/orm.js
+++ config/orm.js
@@ -2,52 +2,65 @@ const connection = require("./connection.js");
 
 
 const orm = {
-    selectAll: function(tableInput){
-        const queryString = `SELECT * FROM ${tableInput};`;
+    selectAll: function(table, callB){
+        const queryString = `SELECT * FROM ${table};`;
         connection.query(queryString, function(error, result){
             if(error){
 
                 console.log(chalk.red(`All-SELECTION COULD NOT BE MADE:\b`));
                 throw error;
-                
-            }
 
+            }
+            callB(result);
         });
 
 
 
     },
-    insertOne: function(){
+    insertOne: function(table, column, val, callB){
+        const queryString = `INSERT INTO ${table}`;
 
+
+        queryString += ` (`;
+        queryString += column.toString();
+        queryString += `) `;
+        queryString += `VALUES (`;
+        queryString += printQuestionMarks(val.length);
+        queryString += `) `;
+
+
+        console.log(chalk.cyan(queryString));
+
+        connection.query(queryString, val, function(error, result){
+
+            if(error){
+                console.log(chalk.red(`INSERT COULD NOT BE EXECUTED: \b`));
+                throw error;
+            }
+            callB(result);
+        });
 
 
     },
-    updateOne: function(){
+    updateOne: function(table, objcolVal, condition, callB){
+        const queryString = `UPDATE ${table}`;
 
+        queryString += ` SET `;
+        queryString += objToSql(objcolVal);
+        queryString += ` WHERE `;
+        queryString += condition;
 
+        console.log(chalk.cyan(queryString));
 
-
+        connection.query(queryString, function(error, result){
+            if(error){
+                console.log(chalk.red(`UPDATE COULD NOT BE EXECUTED: \b`));
+                throw error;
+            }
+            callB(result);
+        });
     }
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 
