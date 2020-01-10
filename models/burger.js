dfac9b758d275865f320ccfe7dd9833b0008c1e0
danman15726 <danman15726@gmail.com>
2020-01-10 15:53:01 -0600

    created the models folder and touched the burger.js file. Also, imported orm into burger.js, created methods derived from the import with a call back response. Used the cat.js file, located in the class repo for reference.

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+const orm = require("../config/orm.js");
+
+
+const burgerFunction = {
+    allSelect: function(callB){
+        orm.all("burgers", function(response){
+            callB(response);
+        });
+    },
+    insert: function(cols, vals, callB){
+        orm.insert("burgers", cols, vals, function(response){
+            callB(response);
+        });
+    },
+    update: function(objcolVal, condition, callB){
+        orm.update("burgers", objcolVal, condition, function(response){
+            callB(response);
+        });
+    }
+
+
+
+
+
+};
+
+module.exports = burgerFunction;
