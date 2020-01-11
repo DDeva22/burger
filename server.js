2e3d5e509c7177fc7f1631be76786fe292758e51
danman15726 <danman15726@gmail.com>
2020-01-10 18:33:36 -0600

    set up the basic handlebars for the data being sent. Trying to understand how the logic works. Using the MvC example from the class repo.

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 const express = require("express");
+const routes = require("./controllers/burgers_controller.js");
+
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+
+
+const eHandlebar = require("express-handlebars");
+app.engine("handlebars", eHandlebar({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log(`APP LISTENING: ${PORT}`);
+});
+
+
+
+
 
