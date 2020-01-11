64cabeffaab8793a57e3b324550cdbb6760f459b
Dan Devarajan <ddevarajan22@gmail.com>
2020-01-11 14:08:33 -0600

    changed connection.js and schema.sql. adjusted for JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,30 @@
 const mysql = require("mysql");
 const chalk = require("chalk");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL){
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
+
+
+
+
+
+
+
+
+
+
 
 connection.connect(function(error){
     if (error) throw error;
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
 
-USE burgers_db;
+
+USE jt3j5k4wr5521jyp;
 
 CREATE TABLE burgers (
 	id INT AUTO_INCREMENT,
